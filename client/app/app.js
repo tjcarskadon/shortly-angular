46f2e9fa00cd6b865816ac0e4a4e36deee939ffc
Tj Carskadon <tjcarskadon@gmail.com>
2016-05-20 22:02:55 -0700

    link nav bar

--- client/app/app.js
+++ client/app/app.js
@@ -15,13 +15,13 @@ angular.module('shortly', [
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
     })
-    .when('/links', {
-      templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
-    })
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
+    })
+    .when('/links', {
+      templateUrl: 'app/links/links.html',
+      controller: 'LinksController'
     });
 
     // We add our $httpInterceptor into the array
--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -5,7 +5,7 @@ angular.module('shortly.auth', [])
 
 .controller('AuthController', function ($scope, $window, $location, Auth) {
   $scope.user = {};
-
+  
   $scope.signin = function () {
     Auth.signin($scope.user)
       .then(function (token) {
