16167dfe843b786674cc021e06b88918c15ddc06
Tj Carskadon <tjcarskadon@gmail.com>
2016-05-20 19:54:01 -0700

    pass all tests

--- karma.conf.js
+++ karma.conf.js
@@ -20,10 +20,10 @@ module.exports = function (config) {
       'client/app/**/*.js',
 
       // our spec files - in order of the README
-      // 'specs/client/authControllerSpec.js',
-      // 'specs/client/servicesSpec.js',
-      // 'specs/client/linksControllerSpec.js',
-      // 'specs/client/shortenControllerSpec.js',
+      'specs/client/authControllerSpec.js',
+      'specs/client/servicesSpec.js',
+      'specs/client/linksControllerSpec.js',
+      'specs/client/shortenControllerSpec.js',
       'specs/client/routingSpec.js'
     ],
 
