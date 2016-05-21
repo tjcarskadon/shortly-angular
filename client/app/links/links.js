8641749608d4dade58d9f0a5a264dee9244c9774
Tj Carskadon <tjcarskadon@gmail.com>
2016-05-20 22:20:17 -0700

    implement logout

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,6 +1,13 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, Auth) {
+ if(!Auth.isAuth()) {
+    Auth.signout();
+
+  }
   $scope.data = {};
   Links.getAll().then(data => $scope.data.links = data);
+
+  $scope.signOut = Auth.signout;
+  
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,8 +1,13 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, Auth) {
+  if(!Auth.isAuth()) {
+    Auth.signout();
+  }
   $scope.link = {};
   $scope.addLink = (link) => {
     Links.addOne(link).then(resp => resp);
   };
+
+  $scope.signOut = Auth.signout;
 });
